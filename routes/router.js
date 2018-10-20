8c858b33d7900951dba8221430fbbee9ff6b7502
Rohit Kumar <33492424+Rohit-Kumarji@users.noreply.github.com>
2018-10-21 01:44:31 +0530

    routes

---
+++ routes/router.js
@@ -0,0 +1,14 @@
+app.post('/create',function(req,res){
+	var subject=req.body.subject;
+	var  topic=req.body.topic;
+	var  location=req.body.location;
+	var  sday=req.body.sday;
+	var  eday=req.body.eday;
+	var  stime=req.body.stime;
+	var  etime=req.body.etime;
+    var newuser=new User({subject:subject,topic:topic,location:location,sday:sday,eday:eday,stime:stime,etime:etime});
+
+	newuser.save(function(err,testEvent){
+	console.log(" is created successfully!");
+})
+});
